55860951b5ec6a7017b23c749827fde2cd587c8c
unknown <baguspgg08@gmail.com>
2023-07-08 12:24:25 +0700

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "Grayish-Blue": "hsl(217, 19%, 38%)",
+        "Dark-Grayish-Blue": "hsl(217, 19%, 24%)",
+        "Dark-Blue": "hsl(218, 23%, 16%)",
+        "Light-Cyan": "hsl(193, 38%, 86%)",
+        "Neo-Green": "hsl(150, 100%, 66%)",
+      },
+      boxShadow: {
+        "3xl": "0 0 40px -2px hsl(150, 100%, 66%)",
       },
     },
   },
   plugins: [],
-}
+};
